8af4a10d20dafbe984c9d2ed8a1fb8be992976ba
jujyne <egmisa.am@gmail.com>
2023-11-15 18:31:36 +0800

    feat(login): add logo and color gradient

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,14 @@
+import logo from '../../assets/images/ChizMiz-logo.png'
+
 export function LoginPage() {
   return (
     <div className="login-cont">
       <form className="login-box">
+        <img src={logo} alt="ChizMiz-logo" />
         <input type="text" placeholder="Email"/>
         <input type="text" placeholder="Password"/>
-        <button>Sign in</button>
+        <button>Login</button>
+        <p>Don't have an account yet? <a href="">Sign Up</a></p>
       </form>
     </div>
   );
