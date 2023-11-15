10d9ae055c709895352a5ec60cc2548603773194
jujyne <egmisa.am@gmail.com>
2023-11-15 15:54:21 +0800

    feat(login): add design to login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,10 +2,8 @@ export function LoginPage() {
   return (
     <div className="login-cont">
       <form className="login-box">
-        <span>EMAIL ADDRESS</span>
-        <input type="text" />
-        <span>PASSWORD</span>
-        <input type="text" />
+        <input type="text" placeholder="Email"/>
+        <input type="text" placeholder="Password"/>
         <button>Sign in</button>
       </form>
     </div>
